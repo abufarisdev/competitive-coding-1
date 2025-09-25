786098857ca6c7a1301ff7d6372f480856c1d951
Abu Faris <abufariskolkata@gmail.com>
2025-09-25 21:19:52 +0530

    add accepting elements parameters

--- maxminarray.c
+++ maxminarray.c
@@ -1,15 +1,31 @@
+//program to display maximun and minimum elements of an array after accepting them from the user
 #include <stdio.h>
 
 int main() {
-    int arr[] = {12, 3, 45, 7, 19, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];   // array of size n
+
+    // Input elements
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Initialize max and min
     int max = arr[0], min = arr[0];
 
+    // Traverse to find max and min
     for (int i = 1; i < n; i++) {
         if (arr[i] > max) max = arr[i];
         if (arr[i] < min) min = arr[i];
     }
 
-    printf("Max = %d\nMin = %d\n", max, min);
+    printf("Maximum = %d\n", max);
+    printf("Minimum = %d\n", min);
+
     return 0;
 }
