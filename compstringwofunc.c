62f1579361374ff2c9e6336cf84d48da9aead213
Abu Faris <abufariskolkata@gmail.com>
2025-11-04 23:50:14 +0530

    add minor changes

--- compstringwofunc.c
+++ compstringwofunc.c
@@ -6,7 +6,7 @@ int main() {
 
     printf("Enter first string: ");
     gets(str1);
-    printf("Enter second string: ");
+    printf("Enter second string:");
     gets(str2);
 
     while (str1[i] != '\0' || str2[i] != '\0') {
